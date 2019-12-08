4774845ec9a9ba48730fc80ac82b24815314fcf2
Andika Andriana <andriana.mia1@gmail.com>
2019-12-08 13:16:03 +0700

    Tugas 20 Javascript
    Tugas ini Saya kumpulkan untuk Niomic.

---
+++ tugas20.js
@@ -0,0 +1,10 @@
+function tugas20() {
+  var kalimat = 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.class';
+
+  console.log(kalimat.match("are"));
+  console.log(kalimat.match("tidak"));
+  console.log(kalimat.match("words"));
+
+}
+
+tugas20();
